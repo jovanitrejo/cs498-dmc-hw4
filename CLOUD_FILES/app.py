94a241cc2acaa6b721b0415473ed3a86166501ab
Jovani Trejo <jtrej7@illinois.edu>
2026-04-17 17:01:14 -0500

    Added remaining Apache Spark endpoints

--- CLOUD_FILES/app.py
+++ CLOUD_FILES/app.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from typing_extensions import LiteralString, cast, List
 import os
 from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+import json
 
 load_dotenv()
 neo4j_password: str | None = os.getenv("NEO4J_PASSWORD")
@@ -11,6 +13,10 @@ URI = "bolt://localhost:7687"
 AUTH = ("neo4j", neo4j_password)
 
 driver = GraphDatabase.driver(URI, auth=AUTH)
+spark = SparkSession.builder.getOrCreate()
+df = spark.read.csv("dataset/taxi_trips_clean.csv", header=True, inferSchema=True)
+df2 = df.withColumn("fare_per_minute", df["fare"] / (df["trip_seconds"] / 60))
+df2.createOrReplaceTempView("taxi_trips_clean")
 app = FastAPI()
 
 @app.get("/graph-summary")
@@ -119,4 +125,68 @@ def get_avg_fares():
 
     return {
         "companies": avg_fares
+    }
+
+@app.get("/area-stats")
+def get_area_stats(area_id: int):
+    area_id_str: str = str(area_id)
+    area_id_lit: LiteralString = cast(LiteralString, area_id_str)
+    query: LiteralString = f"""
+    SELECT dropoff_area AS area_id, COUNT(*) AS trip_count, CAST(ROUND(AVG(fare), 2) AS INT) AS avg_fare, ROUND((AVG(trip_seconds))) AS avg_trip_seconds
+    FROM taxi_trips_clean
+    WHERE dropoff_area = {area_id_lit}
+    GROUP BY dropoff_area 
+    """
+    result = spark.sql(query)
+    json_res = result.toJSON().first()
+    return json.loads(json_res) if json_res else None
+    
+@app.get("/top-pickup-areas")
+def top_areas(n: int):
+    n_str: str = str(n)
+    n_lit: LiteralString = cast(LiteralString, n_str)
+    query: LiteralString = f"""
+    SELECT pickup_area, count(*) AS trip_count
+    FROM taxi_trips_clean
+    GROUP BY pickup_area
+    ORDER BY trip_count DESC
+    LIMIT {n_lit}
+    """
+    result = spark.sql(query)
+    return {
+        "areas": [row.asDict() for row in result.collect()]
+    }
+
+@app.get("/company-compare")
+def compare_companies(company1: str, company2: str):
+    query1: LiteralString = """
+    SELECT company, COUNT(*) AS trip_count, ROUND(AVG(fare),2) AS avg_fare, ROUND(AVG(fare_per_minute),2) AS avg_fare_per_minute, CAST(ROUND(AVG(trip_seconds)) AS INT) AS avg_trip_seconds
+    FROM taxi_trips_clean
+    WHERE company = :company_name
+    GROUP BY company
+    LIMIT 1
+    """
+    query2: LiteralString = """
+    SELECT company, COUNT(*) AS trip_count, ROUND(AVG(fare),2) AS avg_fare, ROUND(AVG(fare_per_minute),2) AS avg_fare_per_minute, CAST(ROUND(AVG(trip_seconds)) AS INT) AS avg_trip_seconds
+    FROM taxi_trips_clean
+    WHERE company = :company_name
+    GROUP BY company
+    LIMIT 1
+    """
+
+    result1 = spark.sql(query1, args={"company_name": company1})
+    result2 = spark.sql(query2, args={"company_name": company2})
+
+    if result1.isEmpty() or result2.isEmpty():
+        return {
+            "error": "one or more companies not found"
+        }
+
+    result1 = result1.first().asDict()
+    result2 = result2.first().asDict()
+    return {
+        "comparison": [
+            result1,
+            result2
+        ]
     }
